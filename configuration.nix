b5937566e30c94b45875ff29f2d6edfc8fdb6b2f
mattyazzz <wideredfoxy@gmail.com>
2026-07-09 06:20:58 -0600

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,114 @@
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "nixos-btw"; # Define your hostname.
+
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Monterrey";
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  services.xserver = {
+	  enable =true;
+	  autoRepeatDelay = 200;
+	  autoRepeatInterval= 35;
+	  windowManager.qtile.enable = true;
+  };
+  services.displayManager.ly.enable = true;
+  
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.graphics.extraPackages = with pkgs; [
+    intel-media-driver
+    intel-vaapi-driver
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.matyazy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  services.flatpak.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+
+    config.common.defailt = "gtk";
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+  	git
+	  kitty
+	  alacritty
+	  flatpak
+    jmtpfs
+    pcmanfm
+    libmtp
+    android-file-transfer
+    vscodium
+  ];
+
+  programs.nix-ld.enable = true;
+
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    openssl
+    xorg.libX11
+    xorg.libXext
+    xorg.libXrender
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libXcursor
+    xorg.libXinerama
+    xorg.libXfixes
+    libGL
+    alsa-lib
+    pulseaudio
+    openal
+    SDL2
+    libogg
+    libvorbis
+    flac
+    mpg123
+  ];
+
+  boot.supportedFilesystems = [ "fuse" ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ]; 
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
+
---
+++ home.nix
@@ -0,0 +1,66 @@
+{ config, pkgs, ... }:
+
+
+{
+    home.username = "matyazy";
+    home.homeDirectory = "/home/matyazy";
+    programs.git.enable = true;
+    home.stateVersion = "26.05";
+    programs.bash = {
+        enable = true;
+        shellAliases = {
+            btw = "echo I use nixos, btw";
+            fast = "command clear | fastfetch";
+            volume = "wpctl set-volume @DEFAULT_AUDIO_SINK@";
+
+            op-home = "command nvim ~/nixos-dotfiles/home.nix";
+            op-config = "command nvim ~/nixos-dotfiles/configuration.nix";
+            op-flake = "command nvim ~/nixos-dotfiles/flake.nix";
+        };
+    };
+    home.file.".config/qtfile".source = ./config/qtile;
+    home.file.".config/nvim".source = ./config/nvim;
+
+    home.sessionPath = [
+      "$HOME/.local/bin"
+];
+
+    home.packages = with pkgs; [
+        neovim
+        ripgrep
+        nil
+        nixfmt-rfc-style
+        nodejs
+        gcc
+        jdk21
+        dummy
+        xrender
+        egl
+        glx
+
+        fastfetch
+        tty-clock
+        cmatrix
+        xdg-user-dirs
+        thunar
+        vscode
+        mesa-demos
+        kcc
+        p7zip
+        file-roller
+        feh
+        unar
+        zip
+        # ani-cli (no sirves, tonto)
+        mpv
+        yt-dlp
+        openssl
+        fzf
+        calibre
+        rofi
+        busybox
+        pulseaudio
+        pavucontrol
+        picom
+    ];
+}
